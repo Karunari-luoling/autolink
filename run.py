fe168ce4fb8209747d77a46231e2c8787424e501
Karunari <thunder_luoling@outlook.com>
2023-10-08 15:23:18 +0800

    更新

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 import os
 
-os.system('nohup python3 -u getData.py > getDate.out 2>&1 &')
+os.system('nohup python3 -u getData.py > getData.out 2>&1 &')
 os.system('nohup python3 -u app.py > app.out 2>&1 &')
