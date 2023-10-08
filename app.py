618ffde19fdd92a8d496e8e8f2ca28b08d944aa9
Karunari <thunder_luoling@outlook.com>
2023-10-08 14:00:27 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,35 @@
+import json
+
+from flask import Flask, jsonify, request
+
+from getData import getbasedb, defold
+
+app = Flask(__name__)
+
+
+@app.before_request
+def before():
+    if not request.path == '/autolink':
+        return jsonify({"code": "正常", "message": "{}".format("输入正确参数")})
+
+
+@app.route('/<json_name>', methods=['GET', 'POST'])
+def read_json(json_name):
+    filename = json_name + '.json'
+    try:
+        with open('./' + filename, encoding='utf-8') as f:
+            autolink = json.load(f)
+            return jsonify(autolink)
+    except Exception as e:
+        return jsonify({"code": "异常", "message": "{}".format(e)})
+
+
+if __name__ == "__main__":
+    try:
+        with open("./config.json",'r') as f:
+            config = json.load(f)
+            port = config['port']
+    except:
+        print("获取配置出错")
+    app.run(debug=False, host='0.0.0.0', port=port)
+
---
+++ getData.py
@@ -0,0 +1,105 @@
+import json
+import os
+import time
+
+import schedule
+import requests
+import urllib3
+
+
+def getbasedb(url):
+    urllib3.disable_warnings()
+    if os.access("./db.json", os.F_OK):
+        os.remove("./db.json")
+    file = requests.get(url, allow_redirects=True, verify=False)
+    open("./db.json", 'wb').write(file.content)
+    processdata()
+
+
+def processdata():
+    for line in open("./db.json", 'r', encoding='utf-8', errors='ignore'):
+        content = json.loads(line)
+        if content['url'] == '/link/':
+            if ' ' in content['comment']:
+                content['comment'] = content['comment'].replace(' ', '')
+            name = content['comment'].split("name:")[1].split("<br>avatar:")[0]
+            avatar = content['comment'].split('avatar:<ahref="')[1].split('">')[0]
+            descr = content['comment'].split("descr:")[1].split('<br>link:')[0]
+            link = content['comment'].split('link:<ahref="')[1].split('">')[0]
+            data = {
+                'mail': content['mail'],
+                'created': content['created'],
+                'name': name,
+                'avatar': avatar,
+                'descr': descr,
+                'link': link
+            }
+            with open("./js_data.json", 'a', encoding='utf-8') as fw:
+                autolink = json.dumps(data, indent=4, ensure_ascii=False)
+                fw.write(autolink + ',')
+                fw.write('\n')
+
+    with open("./js_data.json", 'r', encoding='utf-8') as fw:
+        content = fw.read()
+        if '[' in content:
+            content = content.replace("[", "")
+            content = content.replace("]", ",")
+            content = '[' + content + ']'
+            content = content.replace(",\n]", "]\n")
+        else:
+            content = '[' + content + ']'
+            content = content.replace(",\n]", "]\n")
+    with open("./js_data.json", 'w', encoding='utf-8') as fw:
+        fw.write(content)
+    defold()
+
+
+def defold():
+    json_data = []
+    k, m, n = 0, 0, 0
+    with open("./js_data.json", 'r', encoding='utf-8') as fw:
+        content = fw.read()
+        content = json.loads(content)
+        for i in content:
+            for j in content:
+                m += 1
+                if i['mail'] == j['mail'] and i['created'] == j['created']:
+                    k += 1
+                    n += 1
+                elif i['mail'] == j['mail'] and i['created'] < j['created']:
+                    json_data.append(j)
+                elif i['mail'] == j['mail'] and i['created'] > j['created']:
+                    continue
+                else:
+                    k += 1
+            if k == m and n == 1:
+                json_data.append(i)
+            m, k = 0, 0
+    autolink = json.dumps(json_data, indent=4, ensure_ascii=False)
+    links = []
+    autolink = json.loads(autolink)
+    with open("./autolink.json", 'w', encoding='utf-8') as fw:
+        for link in autolink:
+            if link in links:
+                continue
+            else:
+                links.append(link)
+        autolink = json.dumps(links, indent=4, ensure_ascii=False)
+        fw.write(autolink)
+    os.remove("./js_data.json")
+
+
+if __name__ == "__main__":
+    try:
+        with open("./config.json", 'r') as f:
+            config = json.load(f)
+            url = config['url']
+            port = config['port']
+            interval = config['interval']
+    except:
+        print("获取配置出错")
+    schedule.every(3).hours.do(getbasedb, url)
+    schedule.run_all()
+    while True:
+        schedule.run_pending()
+        time.sleep(60)
