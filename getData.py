f084c4e0e2818c7c4ea6331dc8cf6fd7e69cfb55
Karunari <thunder_luoling@outlook.com>
2023-10-21 15:35:00 +0800

    添加了黑名单链接

--- getData.py
+++ getData.py
@@ -25,28 +25,38 @@ def getbaselocaldb(url):
 
 
 def processdata():
+    with open("./ban.json", 'r', encoding='utf-8') as fw:
+        banurl = fw.read()
+        banurl = json.loads(banurl)
+        banurl = ' '.join(map(str, banurl))
     for line in open("./db.json", 'r', encoding='utf-8', errors='ignore'):
         content = json.loads(line)
         if content['url'] == '/link/':
             if ' ' in content['comment']:
                 content['comment'] = content['comment'].replace(' ', '')
             if 'name' and 'avatar' and 'descr' and 'link' in content['comment']:
+                banlink = ''
                 name = content['comment'].split("name:")[1].split("<br>avatar:")[0]
                 avatar = content['comment'].split('avatar:<ahref="')[1].split('">')[0]
                 descr = content['comment'].split("descr:")[1].split('<br>link:')[0]
                 link = content['comment'].split('link:<ahref="')[1].split('">')[0]
-                data = {
-                    'mail': content['mail'],
-                    'created': content['created'],
-                    'name': name,
-                    'avatar': avatar,
-                    'descr': descr,
-                    'link': link
-                }
-                with open("./js_data.json", 'a', encoding='utf-8') as fw:
-                    autolink = json.dumps(data, indent=4, ensure_ascii=False)
-                    fw.write(autolink + ',')
-                    fw.write('\n')
+                if 'https://' in link:
+                    banlink = link.replace('https:', '').replace('/','')
+                if 'http://' in link:
+                    banlink = link.replace('http:', '').replace('/','')
+                if banlink not in banurl:
+                    data = {
+                        'mail': content['mail'],
+                        'created': content['created'],
+                        'name': name,
+                        'avatar': avatar,
+                        'descr': descr,
+                        'link': link
+                    }
+                    with open("./js_data.json", 'a', encoding='utf-8') as fw:
+                        autolink = json.dumps(data, indent=4, ensure_ascii=False)
+                        fw.write(autolink + ',')
+                        fw.write('\n')
 
     with open("./js_data.json", 'r', encoding='utf-8') as fw:
         content = fw.read()
