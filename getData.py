5646ae6af1171c55f52ff68add9ea9048ad995c5
Karunari <thunder_luoling@outlook.com>
2023-11-14 21:35:58 +0800

    重新修改了一部分代码，暂时关闭了网站测试选项

--- getData.py
+++ getData.py
@@ -7,6 +7,8 @@ import requests
 import urllib3
 from datetime import timezone
 from datetime import datetime
+import itertools
+import operator
 
 
 def getbaseurldb(url):
@@ -24,7 +26,6 @@ def getbaselocaldb(url):
     copy(url, './config/db.json')
     processdata()
 
-
 def processdata():
     if os.path.exists('./config/autolink_back.json'):
         os.remove('./config/autolink_back.json')
@@ -33,9 +34,7 @@ def processdata():
         "dangerous": [],
         "failed": []
     }
-    if not os.path.exists('./config/js_data.json'):
-        with open('./config/js_data.json', 'w', encoding='utf-8') as f:
-            json.dump(basedata, f)
+
     if not os.path.exists('./config/autolink_back.json'):
         with open('./config/autolink_back.json', 'w', encoding='utf-8') as f:
             json.dump(basedata, f)
@@ -76,55 +75,35 @@ def processdata():
                         'descr': descr,
                         'link': link
                     }
-                    with open('./config/js_data.json', 'r', encoding='utf-8') as f:
-                        contents = json.load(f)
-                    contents['partners'].append(data)
-                    with open('./config/js_data.json', 'w', encoding='utf-8') as f:
-                        json.dump(contents, f, indent=4, ensure_ascii=False)
                     with open('./config/autolink_back.json', 'r', encoding='utf-8') as f:
                         contents = json.load(f)
                     contents['partners'].append(data)
                     with open('./config/autolink_back.json', 'w', encoding='utf-8') as f:
                         json.dump(contents, f, indent=4, ensure_ascii=False)
-    if os.path.exists('./config/failed.json'):
-        with open('./config/failed.json', 'r', encoding='utf-8') as f:
-            failed = json.load(f)
-        with open('./config/js_data.json', 'r', encoding='utf-8') as f:
-            contents = json.load(f)
-        contents['failed'] = failed
-        with open('./config/js_data.json', 'w', encoding='utf-8') as f:
-            json.dump(contents, f, indent=4, ensure_ascii=False)
-    if os.path.exists('./dangerous.json'):
-        with open('./config/dangerous.json', 'r', encoding='utf-8') as f:
-            dangerous = json.load(f)
-        with open('./config/js_data.json', 'r', encoding='utf-8') as f:
-            contents = json.load(f)
-        contents['dangerous'] = dangerous
-        with open('./config/js_data.json', 'w', encoding='utf-8') as f:
-            json.dump(contents, f, indent=4, ensure_ascii=False)
-    defold()
-
+    custom()
 
 def defold():
-    with open('./config/js_data.json', 'r', encoding='utf-8') as f:
-        data = json.load(f)
     with open('./config/autolink_back.json', 'r', encoding='utf-8') as f:
-        data_back = json.load(f)
+       data = json.load(f)
+    with open('./config/autolink_back.json', 'r', encoding='utf-8') as f:
+       datas = json.load(f)
 
     def process_list(data, list_name):
-        new_list = [item for item in data[list_name] if not any(
-            other['mail'] == item['mail'] and other['created'] > item['created'] for other in data[list_name])]
+        # 对列表进行排序，具有相同 mail 和 created 的元素会被分到同一组
+        data[list_name].sort(key=operator.itemgetter('mail', 'created'))
+        # 用 itertools.groupby 函数分组
+        groups = itertools.groupby(data[list_name], key=operator.itemgetter('mail', 'created'))
+        # 在每个组中，选择最后一个元素
+        new_list = [list(group)[-1] for key, group in groups]
         return new_list
-
+    
     new_partners = process_list(data, 'partners')
     new_failed = process_list(data, 'failed')
     new_dangerous = process_list(data, 'dangerous')
-    new_partners_back = process_list(data_back, 'partners')
 
     data['partners'] = new_partners
     data['dangerous'] = new_dangerous
     data['failed'] = new_failed
-    data_back['partners'] = new_partners_back
     for item in data['failed']:
         if item in data['partners']:
             data['partners'].remove(item)
@@ -133,92 +112,15 @@ def defold():
     for item in data['dangerous']:
         if item in data['partners']:
             data['partners'].remove(item)
+    with open('./config/autolink_back.json', 'w', encoding='utf-8') as f:
+        json.dump(datas, f, indent=4, ensure_ascii=False)
     with open('./autolink.json', 'w', encoding='utf-8') as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
-    with open('./config/autolink_back.json', 'w', encoding='utf-8') as f:
-        json.dump(data_back, f, indent=4, ensure_ascii=False)
-    os.remove("./config/js_data.json")
-    custom()
     hexo_circle_of_friends()
 
-
-def is_website_alive():
-    if os.path.exists('./config/failed.json'):
-        os.remove('./config/failed.json')
-    contents = []
-    with open('./config/autolink_back.json', encoding='utf-8') as f:
-        datas = json.load(f)
-    links = []
-    for key, value in datas.items():
-        if isinstance(value, list):
-            for item in value:
-                if isinstance(item, dict) and 'link' in item:
-                    links.append(item['link'])
-                    data = {
-                        'mail': item['mail'],
-                        'created': item['created'],
-                        'name': item['name'],
-                        'avatar': item['avatar'],
-                        'descr': item['descr'],
-                        'link': item['link']
-                    }
-                    try:
-                        response = requests.head(item['link'], allow_redirects=True, timeout=5)
-                        if response.status_code == 200:
-                            # print(item['link'] + '状态:' + str(response.status_code))
-                            return True
-                        else:
-                            contents.append(data)
-                            with open('./config/failed.json', 'w', encoding='utf-8') as f:
-                                json.dump(contents, f, indent=4, ensure_ascii=False)
-                    except requests.exceptions.RequestException as e:
-                        print(f"Error occurred: {e}")
-                        contents.append(data)
-                        with open('./config/failed.json', 'w', encoding='utf-8') as f:
-                            json.dump(contents, f, indent=4, ensure_ascii=False)
-
-
-def get_response_time():
-    if os.path.exists('./config/dangerous.json'):
-        os.remove('./config/dangerous.json')
-    if os.path.exists('./config/failed.json'):
-        os.remove('./config/failed.json')
-    contents = []
-    content = []
-    with open('./config/autolink_back.json', encoding='utf-8') as f:
-        datas = json.load(f)
-    links = []
-    for key, value in datas.items():
-        if isinstance(value, list):
-            for item in value:
-                if isinstance(item, dict) and 'link' in item:
-                    links.append(item['link'])
-                    data = {
-                        'mail': item['mail'],
-                        'created': item['created'],
-                        'name': item['name'],
-                        'avatar': item['avatar'],
-                        'descr': item['descr'],
-                        'link': item['link']
-                    }
-                    try:
-                        response = requests.get(url=item['link']).elapsed.total_seconds()
-                        # print(item['link'] + '响应时间:' + str(response))  # 时间为秒
-                        if response > 5:
-                            contents.append(data)
-                            with open('./config/dangerous.json', 'w', encoding='utf-8') as f:
-                                json.dump(contents, f, indent=4, ensure_ascii=False)
-                    except requests.exceptions.RequestException as e:
-                        # print(f"Error occurred: {e}")
-                        content.append(data)
-                        print(content)
-                        with open('./config/failed.json', 'w', encoding='utf-8') as f:
-                            json.dump(content, f, indent=4, ensure_ascii=False)
-
-
 def custom():
-    with open('./autolink.json', 'r', encoding='utf-8') as f:
-        autolink = json.load(f)
+    with open('./config/autolink_back.json', 'r', encoding='utf-8') as f:
+        autolink_back = json.load(f)
     with open('./config/custom.json', 'r', encoding='utf-8') as f:
         customs = json.load(f)
     if customs['partners'] != '[]':
@@ -227,37 +129,36 @@ def custom():
             if 'created' not in partner:  # 如果伙伴没有'created'字段
                 partner['created'] = int(datetime.now(timezone.utc).timestamp() * 1000)  # 获取当前时间戳（毫秒级）并添加到伙伴中
 
-        # 判断autolink中partners的部分
+        # 判断autolink_back中partners的部分
         for partner in customs['partners']:
             email = partner['mail']
             found1 = False
-            found2 = False
-            for autolink_partner in autolink['partners']:
+            for autolink_partner in autolink_back['partners']:
                 if autolink_partner['mail'] == email:
-                    for key in ('siteshot', 'created', 'avatar', 'descr', 'link', 'name'):
+                    for key in ('siteshot', 'created','avatar', 'descr', 'link', 'name'):
                         if partner.get(key):
                             autolink_partner[key] = partner.get(key)
                     found1 = True
-                    break
-            if not found1:  # 如果邮箱不存在于autolink.json的partners中，则添加到autolink.json的partners中
-                for autolink_failed in autolink['failed']:
-                    if autolink_failed['mail'] == email:
-                        found2 = True
-                for autolink_dangerous in autolink['failed']:
-                    if autolink_dangerous['mail'] == email:
-                        found2 = True
-                if not found2:
-                    autolink['partners'].append(partner)
+            if not found1:  # 如果邮箱不存在于autolink_back.json的partners中，则添加到autolink_back.json的partners中
+                autolink_back['partners'].append(partner)
 
+        autolink_back['partners'].sort(key=lambda x: datetime.fromtimestamp(x['created'] / 1000), reverse=False)
 
-
-        autolink['partners'].sort(key=lambda x: datetime.fromtimestamp(x['created'] / 1000), reverse=False)
-
-        with open('./autolink.json', 'w', encoding='utf-8') as f:
-            json.dump(autolink, f, indent=4, ensure_ascii=False)
+        with open('./config/autolink_back.json', 'w', encoding='utf-8') as f:
+            json.dump(autolink_back, f, indent=4, ensure_ascii=False)
         with open('./config/custom.json', 'w', encoding='utf-8') as f:
             json.dump(customs, f, indent=4, ensure_ascii=False)
 
+    if os.path.exists('./config/failed.json'):
+        with open('./config/failed.json', 'r', encoding='utf-8') as f:
+            failed = json.load(f)
+        with open('./config/autolink_back.json', 'r', encoding='utf-8') as f:
+            contents = json.load(f)
+        contents['failed'] = failed
+        with open('./config/autolink_back.json', 'w', encoding='utf-8') as f:
+            json.dump(contents, f, indent=4, ensure_ascii=False)
+    is_website_dangerous()
+    defold()
 
 def hexo_circle_of_friends():
     with open('./autolink.json', 'r', encoding='utf-8') as f:
@@ -268,9 +169,66 @@ def hexo_circle_of_friends():
         avatars = [item['avatar'] for item in autolink['partners']]
         friends = list(zip(names, links, avatars))
         new_data = {'friends': friends}
-        with open('./hexo_circle_of_friends.json', 'w', encoding='utf-8') as f:
-            json.dump(new_data, f, indent=4, ensure_ascii=False)
+    if autolink['dangerous'] != '[]':
+        names = [item['name'] for item in autolink['dangerous']]
+        links = [item['link'] for item in autolink['dangerous']]
+        avatars = [item['avatar'] for item in autolink['dangerous']]
+        dangerous = list(zip(names, links, avatars))
+        new_data["friends"].extend(dangerous)
+    with open('./hexo_circle_of_friends.json', 'w', encoding='utf-8') as f:
+        json.dump(new_data, f, indent=4, ensure_ascii=False)
 
+def is_website_dangerous():
+    with open('./config/custom.json', 'r', encoding='utf-8') as f:
+        customs = json.load(f)
+    if customs['dangerous'] != '[]':
+        with open('./config/autolink_back.json', 'r', encoding='utf-8') as f:
+            dangerous = json.load(f)
+        for partner in dangerous['partners']:  
+            link = partner.get('link') 
+            if link in customs['dangerous']:  
+                dangerous['dangerous'].append(partner)
+        with open('./config/autolink_back.json', 'w', encoding='utf-8') as f:
+            json.dump(dangerous, f, indent=4, ensure_ascii=False)
+
+
+def is_website_alive():
+    if os.path.exists('./config/failed.json'):
+        os.remove('./config/failed.json')
+    contents = []
+    with open('./autolink.json', encoding='utf-8') as f:
+        datas = json.load(f)
+    links = []
+    for key, value in datas.items():
+        if isinstance(value, list):
+            for item in value:
+                if isinstance(item, dict) and 'link' in item:
+                    data = {
+                        'mail': item['mail'],
+                        'created': item['created'],
+                        'name': item['name'],
+                        'avatar': item['avatar'],
+                        'descr': item['descr'],
+                        'link': item['link']
+                    }
+                    try:
+                        print(item['link'])
+                        response = requests.request("GET","https://v2.api-m.com/api/speed?url="+item['link'],headers={'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36'})
+                        response = json.loads(response.text)
+                        if response["code"] == 200:
+                            print(item['link'] + ' 状态:' + str(response["code"]))
+                        else:
+                            print(item['link'] + ' 状态:' + str(response["code"]))
+                            contents.append(data)
+                            with open('./config/failed.json', 'w', encoding='utf-8') as f:
+                                json.dump(contents, f, indent=4, ensure_ascii=False)
+                    except requests.exceptions.RequestException as e:
+                        print(f"Error occurred: {e}")
+                        contents.append(data)
+                        with open('./config/failed.json', 'w', encoding='utf-8') as f:
+                            json.dump(contents, f, indent=4, ensure_ascii=False)
+    print("检测完成")
+    custom()
 
 if __name__ == "__main__":
     try:
@@ -285,16 +243,14 @@ if __name__ == "__main__":
         print("获取配置出错")
     if 'http' in url:
         schedule.every(fentch_interval).minutes.do(getbaseurldb, url)
-        schedule.every(failed_interval).hours.do(is_website_alive)
-        schedule.every(dangerous_interval).hours.do(get_response_time)
+        #schedule.every(failed_interval).hours.do(is_website_alive)
         schedule.run_all()
         while True:
             schedule.run_pending()
             time.sleep(60)
     else:
         schedule.every(fentch_interval).minutes.do(getbaselocaldb, url)
-        schedule.every(failed_interval).hours.do(is_website_alive)
-        schedule.every(dangerous_interval).hours.do(get_response_time)
+        #schedule.every(failed_interval).hours.do(is_website_alive)
         schedule.run_all()
         while True:
             schedule.run_pending()
