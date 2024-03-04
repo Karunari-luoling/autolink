d6a3a1be3826738785a6d87af819805b033282b3
Karunari <thunder_luoling@outlook.com>
2024-03-04 20:13:39 +0800

    添加upload自己提交界面

---
+++ src/app/api/upload.py
@@ -0,0 +1,26 @@
+from datetime import timezone, datetime
+from src.utils.database.read_data import read_data
+from flask import Blueprint, jsonify, request
+import src.utils.config as config
+from src.utils.database.update_data import update_links_data
+
+upload = Blueprint('upload', __name__)
+
+@upload.route('/upload', methods=['POST'])
+def upload_view():
+    banurl = read_data(config.conn, "ban", None, None)
+    banurl = [item[0] for item in banurl]
+    json_data = request.get_json()
+    partners = json_data["partners"]
+    if partners != []:
+        for partner in partners:
+            created = int(datetime.now(timezone.utc).timestamp() * 1000)
+            banlink = partner['link'].replace('https://', '').replace('http://', '').split('/')[0]
+            banlink = '.'.join(banlink.split('.')[-2:])
+            if banlink in banurl:
+                return jsonify({"code": "err", "message": "The link is banned"})
+            data = [partner['name'], partner['avatar'], partner['descr'], partner['link'], partner['siteshot'], "-1", created]
+            mail = partner['mail']
+            update_links_data(config.conn,data,mail)
+        return jsonify({"code": "ok", "message": "Data has been modified successfully"})
+    return jsonify({"code": "err", "message": "There is no modified data"})
--- src/app/app.py
+++ src/app/app.py
@@ -1,3 +1,4 @@
+from src.app.api.upload import upload
 import os
 from flask import Flask, jsonify, request
 from flask_jwt_extended import JWTManager
@@ -24,7 +25,7 @@ def handle_server_error(e):
 
 @app.before_request
 def before():
-    if request.path not in ['/autolink', '/hexo_circle_of_friends', '/custom', '/login', '/test','/config','/delete']:
+    if request.path not in ['/autolink', '/hexo_circle_of_friends', '/custom', '/login', '/test','/config','/delete','/upload'] and request.method != 'OPTIONS':
         return jsonify({"code": "正常", "message": "{}".format("输入正确参数")})
 
 app.register_blueprint(login)
@@ -32,3 +33,4 @@ app.register_blueprint(links_and_friends)
 app.register_blueprint(config)
 app.register_blueprint(custom)
 app.register_blueprint(delete)
+app.register_blueprint(upload)
