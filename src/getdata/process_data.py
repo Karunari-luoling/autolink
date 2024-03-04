c263a9089acc42f5071d4dfe423b1fbd543d4c87
Karunari <thunder_luoling@outlook.com>
2024-03-04 20:16:24 +0800

    获取评论区数据修复

--- src/getdata/process_data.py
+++ src/getdata/process_data.py
@@ -3,6 +3,10 @@ from src.utils.process_decorator import process_decorator
 
 @process_decorator
 def process_data(comment):
+    fields = ['name', 'avatar', 'descr', 'link', 'siteshot']
+    comment = comment.replace('：', ':').replace('&amp;', '&')
+    for field in fields:
+        comment = comment.replace(f'{field}: ', f'{field}:').replace(f'{field}:', f'{field}: ')
     if 'code class' in comment:
         name = get_match(r'name: (.*?)\n', comment)
         avatar = get_match(r'avatar: (.*?)\n', comment)
@@ -12,10 +16,9 @@ def process_data(comment):
     else:
         name = get_match(r'name: (.*?)<br>', comment)
         avatar = get_match(r'avatar: <a href="(.*?)">', comment)
-        descr = get_match(r'descr: (.*?)<br>', comment)
+        descr = get_match(r'descr: (.*?)<', comment)
         link = process_link(get_match(r'link: <a href="(.*?)">', comment))
         siteshot = get_match(r'siteshot: <a href="(.*?)">', comment)
-
     name, avatar, descr, siteshot = process_data_final(name, avatar, descr, siteshot)
 
     return {
